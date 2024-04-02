220236192bb37e686a7b591c8269834853ac552d
rabinson <ra_binson@hotmail.com>
2024-04-02 10:03:03 +0200

    Initial commit'

---
+++ src/main/java/com/pluq/test/PluqApplication.java
@@ -0,0 +1,13 @@
+package com.pluq.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluq/test/PluqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluq.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
