cf52005c8884abfe305fbf4dcc3b7ff8a7bce3b9
rabinson <ra_binson@hotmail.com>
2024-04-02 13:59:14 +0200

    fixing pom

---
+++ src/main/java/com/pluq/PluqApplication.java
@@ -0,0 +1,13 @@
+package com.pluq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluqApplication.class, args);
+	}
+
+}
